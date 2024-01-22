ffb3d81e8c48c4ccb861da12757dafcb08b89007
Abraham Naiborhu <87258755+abrahamparn@users.noreply.github.com>
2024-01-22 20:00:57 +0700

    Adding example data

--- click_indomaret/src/views/MenuSearch.jsx
+++ click_indomaret/src/views/MenuSearch.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import B_Frappe from "../assets/banner/B_Frappe.png";
 import mister_donut from "../assets/menu/mister_donut.png";
@@ -11,8 +11,10 @@ import indomaret from "../assets/indomaret.png";
 import shopping_basket from "../assets/illustration/shopping-basket.png";
 
 import MenuCard from "../parts/MenuCard/MenuCard.jsx";
-
+import { YummyChoice, PointCaffee } from "../data/MenuDataDummy";
 export default function StartingFile() {
+  const [activeMenu, setActiveMenu] = useState("YummyChoice"); // Initial active menu state
+
   let totalProduk = 9;
   let totalHarga = "37.000";
   return (
@@ -20,18 +22,25 @@ export default function StartingFile() {
       <div className="w-full bg-red-500">
         <img src={B_Frappe} className="object-fill bg-black w-full " />
       </div>
-      <div className="grid grid-cols-4 gap-2 mt-4">
-        <div className="col-span-1 flex flex-col justify-center ps-9 pe-9">
+      <div className="grid grid-cols-4 gap-2 mt-4 ">
+        <div className="col-span-1 flex flex-col justify-top ps-9 pe-9">
           <img src={indomaret} className="mb-5" />
-
-          <img src={yummy_choice} className="mb-5" />
+          <img
+            src={yummy_choice}
+            className="mb-5"
+            onClick={() => setActiveMenu("YummyChoice")}
+          />
           <img src={say_bread} className="mb-5" />
           <img src={washoku_sato} className="mb-5" />
           <img src={say_burger} className="mb-5" />
           <img src={mister_donut} className="mb-5" />
-          <img src={point_caffee} className="mb-5" />
+          <img
+            src={point_caffee}
+            className="mb-5"
+            onClick={() => setActiveMenu("PointCaffee")}
+          />
         </div>
-        <div className="col-span-3 flex flex-col">
+        <div className="col-span-3 flex flex-col mb-5">
           <div className="grid grid-cols-3 gap-12">
             <div className="col-span-2">
               <input
@@ -42,12 +51,35 @@ export default function StartingFile() {
             </div>
             <div className="col-span-1">
               <button className="flex items-center h-10 bg-Red_IDM text-white p-1 pe-5 ps-5 rounded-lg active:bg-Blue_IDM">
-                <img src={shopping_basket} alt="Icon" className="mr-2 w-6" />{" "}
+                <img src={shopping_basket} alt="Icon" className="mr-2 w-6" />
                 <strong>Keranjang</strong>
               </button>
             </div>
           </div>
-          <div className="mt-5"></div>
+          <div className="mt-5 ">
+            <div className="grid grid-cols-3  gap-y-6">
+              {activeMenu === "YummyChoice" &&
+                YummyChoice.map((item, index) => (
+                  <MenuCard
+                    key={index}
+                    image={item.image}
+                    name={item.name}
+                    price={item.price}
+                    brand={item.brand}
+                  />
+                ))}
+              {activeMenu === "PointCaffee" &&
+                PointCaffee.map((item, index) => (
+                  <MenuCard
+                    key={index}
+                    image={item.image}
+                    name={item.name}
+                    price={item.price}
+                    brand={item.brand}
+                  />
+                ))}
+            </div>
+          </div>
         </div>
       </div>
       <div className="grid grid-cols-8 border-t-4 border-gray-370">
